e2389ac53e431e2a12e75be0559a5c86e93f0aa5
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-05-02 08:05:53 -0600

    hopefully its right

--- SampleGame/Controller/SampleGame.cs
+++ SampleGame/Controller/SampleGame.cs
@@ -31,6 +31,13 @@ namespace SampleGame.Controller
 
 	// A movement speed for the player
 	private float playerMoveSpeed;
+		private Texture2D projectileTexture;
+		private List<Projectile> projectiles;
+
+		// The rate of fire of the player laser
+		private TimeSpan fireTime;
+		private TimeSpan previousFireTime;
+
 		public SampleGame()
 		{
 			graphics = new GraphicsDeviceManager(this);
@@ -53,6 +60,10 @@ namespace SampleGame.Controller
 			player = new Player();
 			// Set a constant player move speed
 		playerMoveSpeed = 8.0f;
+			projectiles = new List<Projectile>();
+
+			// Set the laser to fire every quarter second
+			fireTime = TimeSpan.FromSeconds(.15f);
 		}
 
 		/// <summary>
@@ -73,7 +84,7 @@ namespace SampleGame.Controller
 						// Load the parallaxing background
 			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
 			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
-
+			projectileTexture = Content.Load<Texture2D>("Texture/laser");
 			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
 
 
@@ -110,8 +121,48 @@ UpdatePlayer(gameTime);
 bgLayer1.Update();
 bgLayer2.Update();
 			base.Update(gameTime);
-		}
+			// Update the projectiles
+UpdateProjectiles();
+			// Projectile vs Enemy Collision
+for (int i = 0; i<projectiles.Count; i++)
+{
+    for (int j = 0; j<enemies.Count; j++)
+    {
+        // Create the rectangles we need to determine if we collided with each other
+        rectangle1 = new Rectangle((int)projectiles[i].Position.X - projectiles[i].Width / 2, (int) projectiles[i].Position.Y - 
+ projectiles[i].Height / 2, projectiles[i].Width, projectiles[i].Height);
 
+        rectangle2 = new Rectangle((int)enemies[j].Position.X - enemies[j].Width / 2, (int) enemies[j].Position.Y - enemies[j].Height / 2, enemies[j].Width, enemies[j].Height);
+
+        // Determine if the two objects collided with each other
+        if (rectangle1.Intersects(rectangle2))
+        {
+            enemies[j].Health -= projectiles[i].Damage;
+            projectiles[i].Active = false;
+        }
+    }
+}
+
+		}
+private void UpdateProjectiles()
+{
+	// Update the Projectiles
+	for (int i = projectiles.Count - 1; i >= 0; i--)
+	{
+		projectiles[i].Update();
+
+		if (projectiles[i].Active == false)
+		{
+			projectiles.RemoveAt(i);
+		}
+ }
+}
+private void AddProjectile(Vector2 position)
+{
+	Projectile projectile = new Projectile();
+	projectile.Initialize(GraphicsDevice.Viewport, projectileTexture, position);
+	projectiles.Add(projectile);
+}
 		/// <summary>
 		/// This is called when the game should draw itself.
 		/// </summary>
@@ -130,7 +181,11 @@ bgLayer2.Update();
 
 			player.Draw(spriteBatch);
 			spriteBatch.End();
-
+			// Draw the Projectiles
+			for (int i = 0; i<projectiles.Count; i++)
+			{
+			    projectiles[i].Draw(spriteBatch);
+			}
 			base.Draw(gameTime);
 		}
 		private void UpdatePlayer(GameTime gameTime)
@@ -161,7 +216,15 @@ player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y* playerMoveSpeed;
     // Make sure that the player does not go out of bounds
     player.Position.X = MathHelper.Clamp(player.Position.X, 0,GraphicsDevice.Viewport.Width - player.Width);
     player.Position.Y = MathHelper.Clamp(player.Position.Y, 0,GraphicsDevice.Viewport.Height - player.Height);
+			// Fire only every interval we set as the fireTime
+			if (gameTime.TotalGameTime - previousFireTime > fireTime)
+			{
+			    // Reset our current time
+			    previousFireTime = gameTime.TotalGameTime;
 
+				// Add the projectile, but add it to the front and center of the player
+				AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
+			}
 		}
 	}
 }
